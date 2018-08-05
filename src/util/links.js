1c6848dcfce09a3c4c276c6e48796ed77d1a3d0e
Leo Zlotnikov <lz375@cam.ac.uk>
2018-08-05 02:12:40 +0100

    Make ESLint happier

--- src/util/links.js
+++ src/util/links.js
@@ -1,5 +1,5 @@
 export default {
-	facebook: "https://www.facebook.com/homertonhtml",
-	github: "https://github.com/Homerton-Thinking-Machine-League",
-	signup: "https://goo.gl/forms/NQdg40uVX7ECKWgf1"
-}
+    facebook: 'https://www.facebook.com/homertonhtml',
+    github: 'https://github.com/Homerton-Thinking-Machine-League',
+    signup: 'https://goo.gl/forms/NQdg40uVX7ECKWgf1',
+};
