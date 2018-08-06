fbc77ede400aabdea4bf9214c7867d75dbb05f04
Leo Zlotnikov <lz375@cam.ac.uk>
2018-08-06 22:30:12 +0100

    Add /api/committee endpoint

--- server/app.py
+++ server/app.py
@@ -1,14 +1,29 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, send_file, redirect, request, abort
 
-react_app_folder = "../build/"
-app = Flask(__name__, static_folder=react_app_folder)
+app = Flask(__name__, static_folder="../build/")
+
+@app.route('/api/committee')
+def get_committee():
+    picture = request.args.get('picture');
+    if picture is not None:
+        if os.path.exists("./data/committee_photos/" + picture):
+            return send_from_directory("./data/committee_photos/", picture), 200
+        return "No pics :-(", 404
+    return send_file("./data/committee.json"), 200
+
+@app.route('/api/<path:path>')
+def unknown_api_endpoint(path):
+    return "Unknown endpoint /api/" + path, 404
 
 # Serve React App
+react_app_folder = "../build/"
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def serve(path):
-    if path != "" and os.path.exists(react_app_folder + path):
-        return send_from_directory(react_app_folder, path)
-    else:
+def serve_react_app(path):
+    print(os.path.exists(react_app_folder + path))
+    if path == "" or not os.path.exists(react_app_folder + path):
+        print("sending index")
         return send_from_directory(react_app_folder, 'index.html')
+    return send_from_directory(react_app_folder, path)
+        
