e5a037140c93d39a5daa1f975b24611e1dd5f84a
Leo Zlotnikov <lz375@cam.ac.uk>
2018-08-06 01:54:10 +0100

    Add dev build script and separate it from start's webpack config

--- config/webpack.config.start.js
+++ config/webpack.config.start.js
@@ -53,7 +53,6 @@ module.exports = {
     // changing JS code would still trigger a refresh.
   ],
   output: {
-    path: paths.appBuild,
     // Add /* filename */ comments to generated require()s in the output.
     pathinfo: true,
     // This does not produce a real file. It's just the virtual path that is
