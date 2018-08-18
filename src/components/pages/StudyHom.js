1feaf723e9a862deaaed4468e4673a3d67094d52
zlotnleo <zlotnleo@gmail.com>
2018-08-18 21:54:18 +0100

    Add Study at Homerton page

--- src/components/pages/StudyHom.js
+++ src/components/pages/StudyHom.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, Paper, Typography } from '@material-ui/core';
+import marked from 'marked';
 
-const StudyHom = () => <div> Study at Homerton Page </div>;
+const StudyHom = props => (
+    <Paper className={props.classes.homePaper}>
 
-export default StudyHom;
+        <Typography
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+                __html: marked(`
+Watch this space, we're going to make some resources to show you why you should come to
+Homerton to study Computer Science.
+                `),
+            }}
+        />
+    </Paper>
+);
+
+StudyHom.propTypes = {
+    classes: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+const styles = theme => ({
+    homePaper: {
+        paddingTop: theme.spacing.unit * 3,
+        paddingBottom: theme.spacing.unit * 3,
+        paddingLeft: theme.spacing.unit * 8,
+        paddingRight: theme.spacing.unit * 8,
+    },
+});
+
+export default withStyles(styles)(StudyHom);
