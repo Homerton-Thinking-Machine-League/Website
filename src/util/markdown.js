a6c7bcb33f993e30d85e9bffc7b7b392bcd1347c
zlotnleo <zlotnleo@gmail.com>
2018-08-23 22:32:08 +0100

    Actually remove markdown code highlighting

--- src/util/markdown.js
+++ src/util/markdown.js
@@ -1,11 +1,7 @@
 import marked from 'marked';
-import pygmentize from 'pygmentize';
 import DOMPurify from 'dompurify';
 
 marked.setOptions({
-    highlight: (code, lang, callback) => {
-        pygmentize({ lang, format: 'html' }, code, (err, res) => callback(err, res));
-    },
     smartLists: true,
 });
 
