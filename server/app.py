2c41023e1e15afdba20dd3f212cbf19ebecfcff8
zlotnleo <zlotnleo@gmail.com>
2018-10-01 16:41:33 +0100

    Remove current event controller because FB Pages API is crap

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, send_from_directory
 from controllers import (
-    news_controller, committee_controller, picture_controller, event_controller
+    news_controller, committee_controller, picture_controller
 )
 
 app = Flask(__name__, static_folder="../build/")
@@ -13,10 +13,6 @@ app.route('/api/committee', endpoint='get_committee', methods=['GET'])(
 
 app.route('/api/news', endpoint='get_news', methods=['GET'])(
     news_controller.get
-)
-
-app.route('/api/events', endpoint='get_events', methods=['GET'])(
-    event_controller.get
 )
 
 app.route('/pictures/<int:id>', endpoint='get_picture_by_id', methods=['GET'])(
