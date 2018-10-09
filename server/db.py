14108098eb3e8f48a6f233164db80e16164e47af
zlotnleo <zlotnleo@gmail.com>
2018-10-09 15:19:56 +0100

    Allow both password and ident postgres authentication

--- server/db.py
+++ server/db.py
@@ -7,11 +7,17 @@ engine = None
 
 with open('config.json') as f:
     config = json.load(f)["database"]
-    engine = create_engine(
-        "postgresql://"
-        + config["username"] + ":" + config["password"]
-        + "@" + config["host"] + ":" + str(config["port"])
-        + "/" + config["database"]
-    )
+
+    use_password = "username" in config and config["username"] != ""
+
+    engine_url = "postgresql://"
+    if use_password:
+        engine_url += config["username"] + ":" + config["password"] + "@"
+    engine_url += config["host"]
+    if "port" in config and config["port"] >= 0:
+        engine_url += ":" + str(config["port"])
+    engine_url += "/" + config["database"]
+
+    engine = create_engine(engine_url)
     connection = engine.connect()
     metadata = MetaData(bind=engine)
